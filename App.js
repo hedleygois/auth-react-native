96a79cd6de3bed50a0160ff917c13d62cfecd92c
Hedley Luna <hedleygois@gmail.com>
2017-07-11 22:55:44 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { Header } from "./src/components/common";
+import LoginForm from "./src/components/LoginForm";
+import firebase from "firebase";
+
+export class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: "AIzaSyDwsblNyR1TxBRxxQhG-ez6xgUbLGttaI4",
+    authDomain: "auth-8efa6.firebaseapp.com",
+    databaseURL: "https://auth-8efa6.firebaseio.com",
+    projectId: "auth-8efa6",
+    storageBucket: "auth-8efa6.appspot.com",
+    messagingSenderId: "145537985188"
+  });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Auth"/>
+        <LoginForm />
+      </View>
+    );
+  }
+}
+
+export default App;
